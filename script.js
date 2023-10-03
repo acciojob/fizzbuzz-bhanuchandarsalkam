9f9ee9931a68505b063378deeb13189f9298fad9
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-10-03 07:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function search(n){
+	for(let i=1;i<=n;i++){
+		if(i%3==0){
+			console.log("Fizz");
+		}
+		else if(i%5==0){
+			console.log("Buzz");
+        }
+		else if(i%3==0&&i%5==0){
+			console.log("FizzBuzz");
+		}
+		else{
+			console.log(i);
+		}
+	}
+}
+alert(search(20));
